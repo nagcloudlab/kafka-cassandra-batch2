d19806336889ff0f56d1468119b356d16815d69b
Nag <nagcloudlab@gmail.com>
2024-05-09 12:46:30 +0530

    day-4 commit

--- live-code/kafka-clients/src/main/java/org/example/KafkaConsumerClient.java
+++ live-code/kafka-clients/src/main/java/org/example/KafkaConsumerClient.java
@@ -1,17 +1,16 @@
 package org.example;
 
 import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
+import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
 public class KafkaConsumerClient {
 
@@ -19,29 +18,108 @@ public class KafkaConsumerClient {
 
     public static void main(String[] args) {
 
-        Properties properties = new Properties();
+        // Properties object to hold all necessary configuration settings
+        Properties props = new Properties();
 
-        properties.put(ConsumerConfig.CLIENT_ID_CONFIG, "consumer-client");
+        // The list of broker addresses in your Kafka cluster
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-group-3");
-        properties.put(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "consumer-instance-1");
+        // Unique string that identifies the consumer group this consumer belongs to
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-group-1");
+        // Unique string that identifies the consumer instance within the consumer group
+        props.put(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "consumer-1");
 
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093");
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
-        properties.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, CooperativeStickyAssignor.class.getName());
+        // Deserializer class for key that implements the Deserializer interface
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        // Deserializer class for value that implements the Deserializer interface
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        // What to do when there is no initial offset in Kafka or if the current offset does not exist any more on the server
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+
+        // If true, the consumer's offset will be periodically committed in the background
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        // The frequency in milliseconds that the consumer offsets are auto-committed to Kafka
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "5000");
+
+        // The maximum number of records returned in a single call to poll()
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "500");
+        // The minimum amount of data the server should return for a fetch request
+        props.put(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, "1");
+        // The maximum amount of time the server will block before answering the fetch request
+        props.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, "500");
+        // The maximum amount of data the server should return per partition
+        props.put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, "1048576"); // 1 MB
+
+        // The expected time between heartbeats to the consumer coordinator
+        props.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, "3000");
+        // The maximum amount of time the group coordinator will wait for each member to send a heartbeat
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "45000");
+
+        // The maximum amount of time the consumer expects a message takes to be processed by the application
+        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, "300000");
+
+        // Partition assignment strategy
+        props.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, CooperativeStickyAssignor.class.getName());
+
+        // Create a KafkaConsumer instance
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(List.of("greetings"), new ConsumerRebalanceListener() {
+            @Override
+            public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+                // This method is called before the rebalance starts and after the consumer stops fetching data
+                // from the partitions it was previously assigned
+
+                // e.g commit the offsets
+
+                // why do we need to commit the offsets here?
+                // because we are about to lose the partitions
+                // we need to commit the offsets before the rebalance starts
+                // so that the next consumer that takes over these partitions knows where to start from
+            }
+            @Override
+            public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+                // This method is called after the consumer has been assigned partitions
+
+                // e.g seek to the last committed offsets
+
+                // why do we need to seek to the last committed offsets here?
+                // because we are about to start fetching data from these partitions
+                // we need to seek to the last committed offsets before we start fetching data
+                // so that we don't miss any messages
+
+            }
+        });
+
+        try {
+            while (true) {
+//              logger.info("Polling");
+                ConsumerRecords<String, String> consumerRecords = consumer.poll(1000);
+                logger.info("Received " + consumerRecords.count() + " records");
+//                Map<TopicPartition, OffsetAndMetadata> currentProcessedOffsets = new HashMap<>();
+//                for (var record : consumerRecords) {
+//                    logger.info("Received new record\nTopic: {}\nKey: {}\nValue: {}\nPartition: {}\nOffset: {}\nTimestamp: {}",
+//                            record.topic(),
+//                            record.key(),
+//                            record.value(),
+//                            record.partition(),
+//                            record.offset(),
+//                            record.timestamp());
+//                    currentProcessedOffsets.put(new TopicPartition(record.topic(), record.partition()), new OffsetAndMetadata(record.offset() + 1));
+                }
+//                consumer.commitSync(currentProcessedOffsets);
+//            }
+        } catch (WakeupException e) {
+            System.out.println("Wake up exception! " + e);
+        } catch (Exception e) {
+            System.out.println("Unexpected exception " + e);
+        } finally {
+            consumer.close(); // Leaving Request to Group Coordinator
+            System.out.println("The consumer is now gracefully closed");
+        }
 
 
-
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
-
-        consumer.subscribe(List.of("greetings"));
-
-        // get a reference to the current thread
         final Thread mainThread = Thread.currentThread();
-        // adding the shutdown hook
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             System.out.println("Detected a shutdown, let's exit by calling consumer.wakeup()...");
             consumer.wakeup();
@@ -53,33 +131,6 @@ public class KafkaConsumerClient {
             }
         }));
 
-        try {
-            while (true) {
-//                 System.out.println("Polling");
-                ConsumerRecords<String, String> consumerRecords = consumer.poll(1000); // 100
-                System.out.println("Received " + consumerRecords.count() + " records");
-                Map<TopicPartition, OffsetAndMetadata> currentProcessedOffsets = new HashMap<>();
-                for (var record : consumerRecords) {
-//                    logger.info("Received new record\nTopic: {}\nKey: {}\nValue: {}\nPartition: {}\nOffset: {}\nTimestamp: {}",
-//                            record.topic(),
-//                            record.key(),
-//                            record.value(),
-//                            record.partition(),
-//                            record.offset(),
-//                            record.timestamp());
-                    currentProcessedOffsets.put(new TopicPartition(record.topic(), record.partition()), new OffsetAndMetadata(record.offset() + 1));
-                }
-                consumer.commitSync(currentProcessedOffsets);
-            }
-
-        } catch (WakeupException e) {
-            System.out.println("Wake up exception! " + e);
-        } catch (Exception e) {
-            System.out.println("Unexpected exception " + e);
-        } finally {
-            consumer.close(); // Leaving Request to Group Coordinator
-            System.out.println("The consumer is now gracefully closed");
-        }
     }
 
 }
---
+++ live-code/kafka-clients/src/main/java/org/example/kafkaStreamsClient_v2.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+import java.util.List;
+import java.util.Properties;
+
+public class kafkaStreamsClient_v2 {
+    public static void main(String[] args) {
+
+
+        Properties consumerProperties=new Properties();
+        consumerProperties.put("bootstrap.servers","localhost:9092");
+        consumerProperties.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+        consumerProperties.put("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+        consumerProperties.put("group.id","test-group");
+
+        Properties producerProperties=new Properties();
+        producerProperties.put("bootstrap.servers","localhost:9092");
+        producerProperties.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+        producerProperties.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+
+
+        KafkaConsumer<String,String> consumer=new KafkaConsumer<String, String>(consumerProperties);
+        consumer.subscribe(List.of("numbers"));
+
+        KafkaProducer<String,String> producer=new KafkaProducer<String, String>(producerProperties);
+
+
+        while(true){
+            var records=consumer.poll(100);
+            records.forEach(record->{
+             String value=record.value();
+                int number=Integer.parseInt(value);
+                if(number%2==0) {
+                    producer.send(new org.apache.kafka.clients.producer.ProducerRecord<String, String>("even-numbers", value));
+                }
+                else{
+                    producer.send(new org.apache.kafka.clients.producer.ProducerRecord<String, String>("odd-numbers", value));
+                }
+            });
+        }
+
+
+    }
+}
